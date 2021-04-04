8f9acb786f371fc93c5e27d2253e185c0a9df51e
Dhiraj Kumar <dhirajkumar31298@gmail.com>
2021-04-04 22:59:43 +0530

    initial commit

---
+++ src/comps/Navbar/MenuItems.js
@@ -0,0 +1,12 @@
+export const MenuItems = [
+  {
+    title: 'Sign in',
+    url: 'https://www.linkedin.com/',
+    cName: 'nav-links'
+  },
+  {
+    title: 'Sign up',
+    url: '#',
+    cName: 'nav-links-mobile'
+  }
+]
