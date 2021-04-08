08d8a6d6ee10d29b9b4668e2e9a134c02b8b6431
Dhiraj Kumar <dhirajkumar31298@gmail.com>
2021-04-08 22:34:05 +0530

    added backend api

---
+++ Backend/app.js
@@ -0,0 +1,33 @@
+var PORT=process.env.PORT||3000;
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/imguploader', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Connected."))
+.catch(err => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const uploadRoute = require('./routes/upload');
+const imagesRoute = require('./routes/images');
+
+app.use('/upload', uploadRoute);
+app.use('/images', imagesRoute);
+
+app.listen(PORT, () => console.log('port 3000'));
+
+
+//happy
+
+
